6116d1e36164c18d08474b8dc8776fbf023788bb
Akhil-Chilakala1997 <ch.akhil222@gmail.com>
2021-10-13 09:19:41 +0530

    'model creation/compilation done'

--- training.py
+++ training.py
@@ -1,12 +1,20 @@
 from  src.utils.common import config_read
 from src.utils.data_mgmt import get_data
+from src.utils.model import create_model
 import argparse
 def training(config_path):
     config = config_read(config_path)
     validation_datasize = config["params"]["validation_datasize"]
     (x_train,y_train),(x_valid,y_valid),(x_test,y_test)=get_data(validation_datasize)
+    LOSS_FUNCTION=config["params"]["loss_function"]
+    OPTIMIZER=config["params"]["optimizer"]
+    METRICS=config["params"]["metrics"]
+    num_classes = config["params"]["num_classes"]
+    model_clf= create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,num_classes)
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION = (x_valid, y_valid)
 
-
+    history = model_clf.fit(x_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
 
 
 
