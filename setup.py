9f49947c304fccf8e06913ad283aeab5c59e35ac
Akhil-Chilakala1997 <ch.akhil222@gmail.com>
2021-10-15 19:04:19 +0530

    deleted setup.py"

--- setup.py
+++
@@ -1,8 +0,0 @@
-with open ("README.md","r",encoding= "utf-8") as f:
-    long_description = f.read()
-setup(
-    name = 'src',
-    version = '0.0.1',
-    author = 'Akhil'
-    
-)
--- src/utils/common.py
+++ src/utils/common.py
@@ -3,3 +3,4 @@ def config_read(config_path):
     with open (config_path) as config:
       content = yaml.safe_load(config)
     return content
+    
